437c1b524c43e3bb7c32a6a3e499faedbf386ffb
jyotiwtm <100362246+jyotiwtm@users.noreply.github.com>
2022-02-25 12:29:33 +0530

    Update register_model.php

--- register_model.php
+++ register_model.php
@@ -1,5 +1,34 @@
 <?php include('./connection.php'); ?>
+<?php 
+if (emptyempty ($_POST["name"])) {  
+    $errMsg = "Error! You didn't enter the Name.";  
+             echo $errMsg;  
+} else {  
+    $name = $_POST["name"];  
+}  
 
+  if (emptyempty($_POST["mobileno"])) {  
+            $mobilenoErr = "Mobile no is required";  
+    } else {  
+            $mobileno = input_data($_POST["mobileno"]);  
+            // check if mobile no is well-formed  
+            if (!preg_match ("/^[0-9]*$/", $mobileno) ) {  
+            $mobilenoErr = "Only numeric value is allowed.";  
+            }  
+        //check mobile no length should not be less and greator than 10  
+        if (strlen ($mobileno) != 10) {  
+            $mobilenoErr = "Mobile no must contain 10 digits.";  
+            }  
+    }  
+$email = $_POST ["Email"];  
+$pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";  
+if (!preg_match ($pattern, $email) ){  
+    $ErrMsg = "Email is not valid.";  
+            echo $ErrMsg;  
+} else {  
+    echo "Your valid email address is: " .$email;  
+}  
+?>
 <?php 
 if(isset($_POST['submit'])){
 
