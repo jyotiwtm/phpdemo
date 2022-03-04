7437bc801dd78fb60b09dd704dfd5fa90eebcb08
jyotiwtm <100362246+jyotiwtm@users.noreply.github.com>
2022-03-04 20:30:19 +0530

    added

---
+++ basics/constant.php
@@ -0,0 +1,10 @@
+<?php 
+
+define("PI", 3.1432);
+
+echo PI;
+
+define("number_array",array(1,2,3,4,5));
+print_r(number_array);
+
+ ?>
---
+++ basics/swapping.php
@@ -0,0 +1,32 @@
+<?php
+$num1 = 10;
+$num2 = 20;
+
+echo " value of num1 and num2 before swapping  num1=$num1 and num2=$num2"; 
+$temp = $num1;
+// echo "temp $num1";
+echo"<br>";
+$num1 = $num2;
+// echo " num1 $num1";
+$num2 = $temp;
+// echo"num2 $num2 ";
+echo"value of num1 and num2 after swapping  num1=$num1 and num2=$num2";
+
+
+// correct varaible names
+$num = 10 ;
+$A ="ABC";
+$password;
+$a="xyz";
+$name_name ="abc";
+$email ="abc@gmail.com";
+$mobile;
+$_is_number = true;
+
+ECHO " A= $A | a = $a ";
+
+// incorrect varaible names
+ // $10;
+// /dd
+
+ ?>
