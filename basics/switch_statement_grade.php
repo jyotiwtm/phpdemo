d4405b2b925c4349d2319bfdedeb0bcf0d13c9ee
jyotiwtm <100362246+jyotiwtm@users.noreply.github.com>
2022-03-09 22:52:34 +0530

    add

---
+++ basics/switch_statement_grade.php
@@ -0,0 +1,13 @@
+<?php 
+$myGrade = "A";
+switch($myGrade){
+    case "A":
+          echo "you pass";
+          break;
+    case "F":
+          echo "You fail";
+          break;
+    default:
+          echo "Invalid grade";
+}
+ ?>
