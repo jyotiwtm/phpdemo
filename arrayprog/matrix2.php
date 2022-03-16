a279c28178c2c1f7921a4406c159fb3c8d8e3822
jyotiwtm <100362246+jyotiwtm@users.noreply.github.com>
2022-03-16 11:04:59 +0530

    add

---
+++ arrayprog/matrix2.php
@@ -0,0 +1,17 @@
+<?php 
+ $arr1 = array(
+ array(2, 2),
+ array(2, 2),
+ 
+);
+
+ echo "Matrix A (2 x 2): <br>";
+for($i=0; $i<2; $i++)
+{
+ for($j=0; $j<2; $j++)
+  echo $arr1[$i][$j] . " ";
+ echo "<br>";
+}
+
+
+ ?>
---
+++ arrayprog/matrix3.php
@@ -0,0 +1,17 @@
+<?php 
+ $arr1 = array(
+ array(3, 3, 3),
+ array(3, 3, 3),
+ array(3, 3, 3),
+);
+
+ echo "Matrix A (3 x 3): <br>";
+for($i=0; $i<3; $i++)
+{
+ for($j=0; $j<3; $j++)
+  echo $arr1[$i][$j] . " ";
+ echo "<br>";
+}
+
+
+ ?>
