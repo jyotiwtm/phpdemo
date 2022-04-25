08af8d4eab8a919280bbf8850d0ffe61b48c063b
jyotiwtm <100362246+jyotiwtm@users.noreply.github.com>
2022-04-25 20:27:15 +0530

    add

---
+++ mysql/sum_agregate.php
@@ -0,0 +1,19 @@
+<?php 
+include '../connection.php';
+include '../header.php';
+
+$query = "SELECT sum(remaining_amount) AS wallet FROM `transaction` WHERE uid=1000 and txn_mode = 0";
+
+$result = mysqli_query($connection,$query);
+
+if(mysqli_num_rows($result) > 0){
+
+$row = mysqli_fetch_object($result);
+if(!empty($row->wallet)){
+	echo "Wallet = $row->wallet ";	
+} else {
+	echo "Wallet = 0 ";
+}
+
+}
+ ?>
